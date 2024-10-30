f2a77864b37de040b5e5eb26766f6f63394a76fa
Jonathan Rodriguez <jonathan@192.168.2.23>
2024-10-30 16:31:34 -0500

    actualizacion5Inscription

--- src/components/PublicRecordCreate.jsx
+++ src/components/PublicRecordCreate.jsx
@@ -19,58 +19,69 @@ export default function PublicRecordCreate() {
   const [files, setFiles] = useState([]);
   const [fileNames, setFileNames] = useState({});
 
+  // Función para normalizar los nombres de los campos
+  const normalize = (str) => {
+    return str
+      .toLowerCase()
+      .normalize("NFD") // Normaliza los caracteres unicode
+      .replace(/[\u0300-\u036f]/g, "") // Elimina los acentos
+      .replace(/[^a-z0-9]/g, '_') // Reemplaza caracteres especiales por guiones bajos
+      .replace(/_+/g, '_') // Reemplaza múltiples guiones bajos por uno solo
+      .trim();
+  };
+
   // Mapeo de nombres personalizados para los campos
   const fieldLabels = {
-    "nombres": "Nombres",
-    "apellidos": "Apellidos",
-    "tipo_de_identificacion": "Tipo de identificación",
-    "numero_de_identificacion": "Número de identificación",
-    "fecha_de_nacimiento": "Fecha de nacimiento (DD/MM/AAAA)",
-    "edad": "Edad",
-    "sexo": "Sexo",
-    "telefono_fijo": "Teléfono fijo",
-    "celular": "Celular",
-    "celular_2": "Celular 2 (puede ser el de un familiar)",
-    "correo_electronico": "Correo electrónico",
-    "direccion": "Dirección de residencia",
-    "barrio": "Barrio de residencia",
-    "localidad_de_residencia": "Localidad de residencia",
-    "nivel_educativo_del_empresario": "Nivel educativo del empresario",
-    "presenta_algun_tipo_de_discapacidad": "¿Presenta algún tipo de discapacidad?",
-    "grupo_etnico": "Grupo étnico",
-    "es_victima_del_conflicto_armado": "¿Es víctima del conflicto armado?",
-    "es_cuidador_de_alguien": "¿Es cuidador de alguna de las siguientes personas?",
-    "identidad_de_genero": "Identidad de género",
-    "personas_a_cargo": "Personas a cargo",
-    "nombre_del_emprendimiento": "Nombre de la unidad de negocio",
-    "fecha_de_inicio_actividad_economica": "Fecha de inicio actividad económica",
-    "esta_registrado_y_renovado_ante_la_camara_de_comercio": "¿Su unidad de negocio está registrado ante la Cámara de Comercio?",
-    "logro_renovar_la_matricula_del_negocio_a_comienzos_del_2023": "¿Logró renovar la matrícula del negocio a comienzos del 2024?",
-    "fecha_de_registro_en_camara_de_comercio": "Fecha de registro en Cámara de Comercio (DD/MM/AAAA)",
-    "nit": "NIT (sin dígito de verificación)",
-    "localidad_de_la_unidad_de_negocio": "Localidad de la unidad de negocio",
-    "direccion_de_la_unidad_de_negocio": "Dirección en donde se desarrolla la actividad de la unidad de negocio (debe coincidir con el servicio público que va a adjuntar más adelante)",
-    "es_tambien_su_vivienda": "¿En esta dirección también es su vivienda?",
-    "barrio_de_la_unidad_de_negocio": "Barrio de la unidad de negocio",
-    "telefono_fijo_de_la_unidad_de_negocio": "Teléfono fijo de la unidad de negocio",
-    "area_del_negocio": "¿El negocio se encuentra ubicado en área?",
-    "estrato_socioeconomico": "Estrato socioeconómico de su unidad de negocio",
-    "tiempo_de_funcionamiento": "¿Cuánto tiempo de funcionamiento tiene su unidad de negocio?",
-    "es_vendedor_informal_o_ambulante": "¿Usted es vendedor informal/ambulante registrado en el HEMI con RIVI de la localidad por la cual usted se postula?",
-    "personas_en_el_emprendimiento": "¿Cuántas personas trabajan directamente en su unidad de negocio, incluyéndolo a usted?",
-    "sector_productivo": "¿En qué sector productivo se encuentra su unidad de negocio?",
-    "oferta_productos_o_servicios": "¿Cuál es la oferta de productos o servicios de su unidad de negocio?",
-    "actividades_sostenibles": "¿Su unidad de negocio realiza actividades sostenibles y en proceso de reconversión dirigidas al cuidado del medio ambiente?",
-    "actividad_implementada": "¿Cuál es esa actividad que Ud. implementa que es sostenible y en proceso de reconversión dirigidas al cuidado del medio ambiente?",
-    "acceso_a_internet": "¿Tiene acceso a internet y/o a un dispositivo que le permita acceder a las cápsulas de conocimiento?",
-    "plan_datos_celular": "¿Cuenta con plan de datos en su celular?",
-    "cuenta_bancaria": "¿Dispone de una cuenta bancaria o algún servicio de billetera electrónica que le permita recibir el incentivo económico?",
-    "cual": "¿Cuál?",
-    "clientes_actuales": "Número de clientes actuales",
-    "ventas_promedio_mensual": "Valor de ventas promedio mensual",
-    "tiempo_para_formacion": "¿De cuánto tiempo dispone para dedicarle al proceso de formación y realización del plan de inversión?",
-    "canales_comercializacion": "Para la comercialización de su producto utiliza canales como:",
-    "funcionario_publico": "¿El dueño del emprendimiento es funcionario público?"
+    [normalize("Nombres")]: "Nombres",
+    [normalize("Apellidos")]: "Apellidos",
+    [normalize("Tipo de identificacion")]: "Tipo de identificación",
+    [normalize("Numero de identificacion")]: "Número de identificación",
+    [normalize("Fecha de nacimiento")]: "Fecha de nacimiento (DD/MM/AAAA)",
+    [normalize("Edad")]: "Edad",
+    [normalize("Sexo")]: "Sexo",
+    [normalize("Telefono fijo")]: "Teléfono fijo",
+    [normalize("Celular")]: "Celular",
+    [normalize("Celular 2")]: "Celular 2 (puede ser el de un familiar)",
+    [normalize("Correo electronico")]: "Correo electrónico",
+    [normalize("Direccion")]: "Dirección de residencia",
+    [normalize("Barrio")]: "Barrio de residencia",
+    [normalize("Localidad de residencia")]: "Localidad de residencia",
+    [normalize("Nivel educativo del empresario")]: "Nivel educativo del empresario",
+    [normalize("Presenta algun tipo de discapacidad")]: "¿Presenta algún tipo de discapacidad?",
+    [normalize("Grupo etnico")]: "Grupo étnico",
+    [normalize("Es victima del conflicto armado")]: "¿Es víctima del conflicto armado?",
+    [normalize("Es cuidador de alguna de las siguientes personas")]: "¿Es cuidador de alguna de las siguientes personas?",
+    [normalize("Identidad de genero")]: "Identidad de género",
+    [normalize("Personas a cargo")]: "Personas a cargo",
+    [normalize("Nombre del emprendimiento")]: "Nombre de la unidad de negocio",
+    [normalize("Fecha de inicio actividad economica")]: "Fecha de inicio actividad económica",
+    [normalize("Esta registrado y renovado ante la Camara de Comercio")]: "¿Su unidad de negocio está registrado ante la Cámara de Comercio?",
+    [normalize("Logro renovar la matricula del negocio a comienzos del 2023")]: "¿Logró renovar la matrícula del negocio a comienzos del  2024?",
+    [normalize("Fecha de registro en Cámara de Comercio")]: "Fecha de registro en Cámara de Comercio (DD/MM/AAAA)",
+    [normalize("NIT")]: "NIT (sin dígito de verificación)",
+    [normalize("Localidad de la unidad de negocio")]: "Localidad de la unidad de negocio",
+    [normalize("Direccion de la unidad de negocio")]: "Dirección en donde se desarrolla la actividad de la unidad de negocio (debe coincidir con el servicio público que va a adjuntar más adelante)",
+    [normalize("En esta direccion tambien es su vivienda")]: "¿En esta dirección también es su vivienda?",
+    [normalize("Barrio de la unidad de negocio")]: "Barrio de la unidad de negocio",
+    [normalize("Telefono fijo de la unidad de negocio")]: "Teléfono fijo de la unidad de negocio",
+    [normalize("El negocio se encuentra ubicado en area")]: "¿El negocio se encuentra ubicado en área?",
+    [normalize("Estrato socioeconomico de su unidad de negocio")]: "Estrato socioeconómico de su unidad de negocio",
+    [normalize("Cuanto tiempo de funcionamiento tiene su emprendimiento")]: "¿Cuánto tiempo de funcionamiento tiene su unidad de negocio?",
+    [normalize("Vendedor informal o ambulante registrado en el HEMI con RIVI")]: "¿Usted es vendedor informal/ambulante registrado en el HEMI con RIVI de la localidad por la cual usted se postula?",
+    [normalize("Cuantas personas trabajan directamente en el emprendimiento")]: "¿Cuántas personas trabajan directamente en su unidad de negocio, incluyéndolo a usted?",
+    [normalize("En que sector productivo se encuentra su emprendimiento")]: "¿En qué sector productivo se encuentra su unidad de negocio?",
+    [normalize("Cual es la oferta de productos o servicios de su negocio")]: "¿Cuál es la oferta de productos o servicios de su unidad de negocio?",
+    [normalize("Realiza actividades sostenibles y en proceso de reconversion")]: "¿Su unidad de negocio realiza actividades sostenibles y en proceso de reconversión dirigidas al cuidado del medio ambiente?",
+    [normalize("Actividad que Ud. Implementa sostenible y de reconversion")]: "¿Cuál es esa actividad que Ud. implementa que es sostenible y en proceso de reconversión dirigidas al cuidado del medio ambiente?",
+    [normalize("Tiene acceso a internet y a un dispositivo")]: "¿Tiene acceso a internet y/o a un dispositivo que le permita acceder a las cápsulas de conocimiento?",
+    [normalize("Cuenta con plan de datos en su celular")]: "¿Cuenta con plan de datos en su celular?",
+    [normalize("Dispone de una cuenta bancaria o billetera electronica")]: "¿Dispone de una cuenta bancaria o algún servicio de billetera electrónica que le permita recibir el incentivo económico?",
+    [normalize("Cual")]: "¿Cuál?",
+    [normalize("Numero de clientes actuales")]: "Número de clientes actuales",
+    [normalize("Valor de ventas promedio mensual")]: "Valor de ventas promedio mensual",
+    [normalize("Cuanto tiempo dispone para el proceso de formacion y PI")]: "¿De cuánto tiempo dispone para dedicarle al proceso de formación y realización del plan de inversión?",
+    [normalize("Para la comercializacion de su producto utiliza canales como")]: "Para la comercialización de su producto utiliza canales como:",
+    [normalize("El dueño del emprendimiento es funcionario publico")]: "¿El dueño del emprendimiento es funcionario público?"
   };
 
   // Obtener los campos de la tabla y datos relacionados
@@ -80,6 +91,8 @@ export default function PublicRecordCreate() {
         const fieldsResponse = await axios.get(
           `https://impulso-local-back.onrender.com/api/inscriptions/tables/${tableName}/fields`
         );
+
+        console.log('Campos obtenidos:', fieldsResponse.data);
 
         // Filtrar los campos para excluir 'Estado', 'Asesor' e 'ID'
         const filteredFields = fieldsResponse.data.filter(
@@ -93,6 +106,7 @@ export default function PublicRecordCreate() {
         setRelatedData(relatedDataResponse.data.relatedData || {});
         setLoading(false);
       } catch (error) {
+        console.error('Error obteniendo los datos de los campos:', error);
         setError('Error obteniendo los datos de los campos');
         setLoading(false);
       }
@@ -140,6 +154,7 @@ export default function PublicRecordCreate() {
         }
       );
 
+      // Verificar que el registro se haya creado correctamente y que contenga un 'id'
       const createdRecordId = recordResponse.data?.record?.id || recordResponse.data?.id;
 
       if (!createdRecordId) {
@@ -165,6 +180,7 @@ export default function PublicRecordCreate() {
           );
         });
 
+        // Esperar a que todas las subidas de archivos finalicen
         await Promise.all(uploadPromises);
       }
 
@@ -198,34 +214,43 @@ export default function PublicRecordCreate() {
             <div>Cargando...</div>
           ) : (
             <form onSubmit={handleSubmit}>
-              {fields.map((field) => (
-                <div className="form-group" key={field.column_name}>
-                  <label>{fieldLabels[field.column_name] || field.column_name}</label>
-                  {Array.isArray(relatedData[field.column_name]) ? (
-                    <select
-                      className="form-control"
-                      name={field.column_name}
-                      value={newRecord[field.column_name] || ''}
-                      onChange={handleChange}
-                    >
-                      <option value="">-- Selecciona una opción --</option>
-                      {relatedData[field.column_name].map((relatedRecord) => (
-                        <option key={relatedRecord.id} value={relatedRecord.id}>
-                          {relatedRecord.displayValue || `ID: ${relatedRecord.id}`}
-                        </option>
-                      ))}
-                    </select>
-                  ) : (
-                    <input
-                      type="text"
-                      name={field.column_name}
-                      value={newRecord[field.column_name] || ''}
-                      onChange={handleChange}
-                      className="form-control"
-                    />
-                  )}
-                </div>
-              ))}
+              {fields.map((field) => {
+                const normalizedColumnName = normalize(field.column_name);
+
+                // Excluir el campo 'id' por si acaso
+                if (normalizedColumnName === 'id') {
+                  return null;
+                }
+
+                return (
+                  <div className="form-group" key={field.column_name}>
+                    <label>{fieldLabels[normalizedColumnName] || field.column_name}</label>
+                    {Array.isArray(relatedData[field.column_name]) ? (
+                      <select
+                        className="form-control"
+                        name={field.column_name}
+                        value={newRecord[field.column_name] || ''}
+                        onChange={handleChange}
+                      >
+                        <option value="">-- Selecciona una opción --</option>
+                        {relatedData[field.column_name].map((relatedRecord) => (
+                          <option key={relatedRecord.id} value={relatedRecord.id}>
+                            {relatedRecord.displayValue || `ID: ${relatedRecord.id}`}
+                          </option>
+                        ))}
+                      </select>
+                    ) : (
+                      <input
+                        type="text"
+                        name={field.column_name}
+                        value={newRecord[field.column_name] || ''}
+                        onChange={handleChange}
+                        className="form-control"
+                      />
+                    )}
+                  </div>
+                );
+              })}
 
               <div className="form-group">
                 <label>Seleccionar archivos para subir</label>
